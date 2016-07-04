96caaf50e335a6ba99704bd1e5da8c82fef76761
Dmitriy Kaminskiy <dmitriy.kaminskiy@matrix42.com>
2016-07-04 15:17:45 +0300

    up

--- directives/mxServicePicker.directive.js
+++ directives/mxServicePicker.directive.js
@@ -3,7 +3,8 @@
 
     angular
         .module('app')
-        .directive('mxServicePicker', mxServicePicker);
+        .directive('mxServicePicker', mxServicePicker)
+        .directive('clearAutocompleteBtn', clearAutocompleteBtn)
 
     function mxServicePicker() {
         var ddo = {
@@ -22,6 +23,54 @@
         };
 
         return ddo;
+    }
+
+    function clearAutocompleteBtn($parse, $compile) {
+        var ddo = {
+            restrict: 'A',
+            link: link
+        };
+
+        return ddo;
+
+        function link(scope, element, attrs) {
+
+            if (!attrs.mdFloatingLabel) return;
+
+            var template = [
+                '<md-button ng-hide="vm.disabled || vm.readOnly" tabindex="-1" class="md-icon-button clear-autocomplete">',
+                '<md-icon md-svg-icon="md-close">',
+                '</md-icon>',
+                '</md-button>'
+            ].join('');
+
+            var linkFn = $compile(template);
+            var button = linkFn(scope);
+            element.append(button);
+
+            var searchTextModel = $parse(attrs.mdSearchText);
+
+            scope.$watch(searchTextModel, function(searchText) {
+                if (searchText && searchText !== '' && searchText !== null) {
+                    button.addClass('visible');
+                } else {
+                    button.removeClass('visible');
+                }
+            });
+
+            button.on('click', onClick);
+
+            scope.$on('$destroy', function() {
+                button.off('click');
+            });
+
+            function onClick() {
+                searchTextModel.assign(scope, undefined);
+                scope.$digest();
+                angular.element(document.querySelectorAll('.autocomplete-popover')).remove();
+                element.removeClass('valid-value');
+            }
+        }
     }
 
 })(window);
