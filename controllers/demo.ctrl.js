2fac3dd3797d200a6607289a0494dd2e6ab7e574
Dmitriy Kaminskiy <dmitriy.kaminskiy@matrix42.com>
2016-07-05 16:16:00 +0300

    add typeahead data

--- controllers/demo.ctrl.js
+++ controllers/demo.ctrl.js
@@ -8,9 +8,13 @@
     function DemoCtrl(ServicePicker) {
         var vm = this;
 
+
         vm.isDisabled = false;
         vm.isReadOnly = false;
         vm.isRequired = true;
+
+        vm.selected = undefined;
+        vm.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
         vm.mode  = 'service';
         vm.modes = ['service', 'operation'];
