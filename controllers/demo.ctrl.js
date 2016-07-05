3d79dd6e02b3da9d542fd8bcb827126b0e6af876
Dmitriy Kaminskiy <dmitriy.kaminskiy@matrix42.com>
2016-07-05 18:04:00 +0300

    add additional picker type

--- controllers/demo.ctrl.js
+++ controllers/demo.ctrl.js
@@ -5,7 +5,7 @@
         .module('app')
         .controller('DemoCtrl', DemoCtrl)
 
-    function DemoCtrl(ServicePicker) {
+    function DemoCtrl($http, ServicePicker) {
         var vm = this;
 
 
@@ -15,6 +15,21 @@
 
         vm.selected = undefined;
         vm.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
+        vm.getLocation = function(val) {
+
+            return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+                params: {
+                    address: val,
+                    sensor: false
+                }
+            }).then(function(response) {
+                return response.data.results.map(function(item) {
+                    return item.formatted_address;
+                });
+            });
+
+        };
 
         vm.mode  = 'service';
         vm.modes = ['service', 'operation'];
