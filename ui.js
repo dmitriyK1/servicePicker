6c2c6ab9420e830555539ea06fe7d18e8bdb87e8
Dmitriy Kaminskiy <dmitriy.kaminskiy@matrix42.com>
2016-07-07 13:06:12 +0300

    up

--- ui.js
+++ ui.js
@@ -1,27 +1,35 @@
-$('#submit').click(function onClick(e) {
-
-    var keyword   = $('#keyword').val();
-    var hostId    = $('#hostId').val();
-    var serviceId = $('#serviceId').val();
-    var mode      = $('input[name="mode"]:checked').val();
-
-    if (mode === 'services') {
-        var result = search({
-            hostId  : hostId,
-            keyword : keyword,
-            mode    : mode
+    $(function() {
+        $('#toggle-cases').click(function() {
+            $('#cases').slideToggle();
         });
-    }
+    });
 
-    if (mode === 'operations') {
-        var result = search({
-            hostId    : hostId,
-            serviceId : serviceId,
-            keyword   : keyword,
-            mode      : mode
-        });
-    }
 
-    $('#output').html( JSON.stringify(result) );
+    $('#submit').click(function onClick(e) {
 
-});
+
+        var keyword = $('#keyword').val();
+        var hostId = $('#hostId').val();
+        var serviceId = $('#serviceId').val();
+        var mode = $('input[name="mode"]:checked').val();
+
+        if (mode === 'services') {
+            var result = search({
+                hostId: hostId,
+                keyword: keyword,
+                mode: mode
+            });
+        }
+
+        if (mode === 'operations') {
+            var result = search({
+                hostId: hostId,
+                serviceId: serviceId,
+                keyword: keyword,
+                mode: mode
+            });
+        }
+
+        $('#output').html(JSON.stringify(result));
+
+    });
