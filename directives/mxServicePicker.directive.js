6a1a40c6aa5a27bcbef2fb038862fb3e60e45371
Dmitriy Kaminskiy <dmitriy.kaminskiy@matrix42.com>
2016-07-13 10:03:21 +0300

    add readonly mode

--- directives/mxServicePicker.directive.js
+++ directives/mxServicePicker.directive.js
@@ -5,24 +5,59 @@
         .module('app')
         .directive('mxServicePicker', mxServicePicker)
 
-    function mxServicePicker() {
+    function mxServicePicker($mdConstant) {
         var ddo = {
-            restrict    : 'E',
-            controller  : 'MxServicePickerCtrl as vm',
-            templateUrl : 'directives/mxServicePicker.directive.html',
-            scope       : {},
+            restrict: 'E',
+            controller: 'MxServicePickerCtrl as vm',
+            templateUrl: 'directives/mxServicePicker.directive.html',
+            scope: {},
 
-            bindToController : {
-                disabled : '=ngDisabled',
-                label    : '@',
-                mode     : '@',
+            bindToController: {
+                disabled: '=ngDisabled',
+                label: '@',
+                mode: '@',
                 // model    : '=ngModel',
-                required : '='
-            }
+                required: '=',
+                readOnly: '='
+            },
+
+            link: link
 
         };
 
         return ddo;
+
+        function link(scope, element, attrs, ctrl) {
+            element.on('keydown', onKeyDown);
+
+            function onKeyDown(e) {
+                var isCtrlPressed = e.ctrlKey;
+                var isShiftPressed = e.shiftKey;
+                var isXPressed = e.keyCode === 88;
+                var isVPressed = e.keyCode === 86;
+                var isTabPressed = e.keyCode === $mdConstant.KEY_CODE.TAB;
+                var isDeletePressed = e.keyCode === $mdConstant.KEY_CODE.DELETE;
+                var isBackspacePressed = e.keyCode === $mdConstant.KEY_CODE.BACKSPACE;
+                var isMovementKeyPressed = e.keyCode === $mdConstant.KEY_CODE.UP_ARROW ||
+                    e.keyCode === $mdConstant.KEY_CODE.DOWN_ARROW ||
+                    e.keyCode === $mdConstant.KEY_CODE.LEFT_ARROW ||
+                    e.keyCode === $mdConstant.KEY_CODE.RIGHT_ARROW ||
+                    e.keyCode === $mdConstant.KEY_CODE.HOME ||
+                    e.keyCode === $mdConstant.KEY_CODE.END;
+
+                if (scope.vm.readOnly) {
+                    if (isDeletePressed || isBackspacePressed || isXPressed || isVPressed) {
+                        e.preventDefault();
+                    }
+
+                    if (!isCtrlPressed && !isTabPressed && !isMovementKeyPressed) {
+                        e.preventDefault();
+                    }
+                }
+
+            }
+
+        }
 
     }
 
